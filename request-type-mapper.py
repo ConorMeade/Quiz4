840650d53e7a33b0caecd8d6961982ba4b405b3b
Conor Meade <cmeade6479@gmail.com>
2024-10-12 14:08:20 -0400

    part 2 comments, request type mapper and reducer

---
+++ request-type-mapper.py
@@ -0,0 +1,15 @@
+import sys
+
+def mapper():  
+    for line in sys.stdin:
+        # split on opening parens, second element will be the request type
+        log_elems = line.split('"')
+        if len(log_elems) > 1:
+            # print(log_elems)
+            # break
+            # after date time portion, we have the GET, POST, PUT etc. message
+            request = log_elems[1].split()  # ['GET', '/index.php?option=com_phocagallery&view=category&id=1:almhuette-raith&Itemid=53', 'HTTP/1.1']
+            # ensure we actually have data
+            if len(request) > 0:
+                method = request[0]
+                # print(f"{method}\t1")
---
+++ request-type-reducer.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def reducer():
+    request_count = {}
+
+
+    for line in sys.stdin:
+        method, count = line.strip().split('\t')
+        request_count[method] += int(count)
+
+    total_reqs = sum(request_count.values())
+
+    for method, count in request_count.items():
+        percentage_of_reqs = (count / total_reqs) * 100
+        print(f"{method}\t{percentage_of_reqs:.2f}%")
