d82b746db3c7bbb94118806a97370ef45870b1d1
Conor Meade <cmeade6479@gmail.com>
2024-10-12 14:59:10 -0400

    update mapper

--- request-type-mapper.py
+++ request-type-mapper.py
@@ -1,9 +1,26 @@
 import sys
 
-def main(argv):
 
+# def main(argv):
+#     line = sys.stdin.readline()
+#     pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
+#     try:
+#         while line:
+#             for word in pattern.findall(line):
+#                 print ("LongValueSum:" + word.lower() + "\t" + "1")
+#                 # x = 1 / random.randint(0,99)
+#             line = sys.stdin.readline()
+#     except EOFError as error:
+#         return None
+
+# if __name__ == "__main__":
+#     main(sys.argv)
+
+def main(argv):
+    line = sys.stdin.readline()
     try:
-        for line in sys.stdin:
+        # for line in sys.stdin:
+        while line:
             # split on opening parens, second element will be the request type
             log_elems = line.split('"')
             if len(log_elems) > 1:
@@ -15,6 +32,8 @@ def main(argv):
                 if len(request) > 0:
                     method = request[0]
                     print(f"{method}\t1")
+        line = sys.stdin.readline()
+                
 
     except EOFError as error:
         return None
