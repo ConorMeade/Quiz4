222aa87e9a5efe57646638bc92ed43cc4a3e5fa0
Conor <cmeade6479@gmail.com>
2024-10-15 01:21:01 -0400

    update reducer

--- prez_speeches_reducer.py
+++ prez_speeches_reducer.py
@@ -3,25 +3,30 @@
 '''prez_speeches_mapper.py'''
 import sys
 
-total_valence_score = {}
-total_valence_words = {}
+valence_aggregate = {}
+valence_words_ct = {}
 
 for line in sys.stdin:
     president_name, valence_score = line.strip().split('\t', 1)
     try:
-        covalence_scoreunt  = int(valence_score)
+        valence_score  = int(valence_score)
     except ValueError:
         # count was not a number, so silently
         # ignore/discard this line
         continue
-    if president_name not in total_valence_score:
-        total_valence_score[president_name] = 0
+    if president_name not in valence_aggregate:
+        valence_aggregate[president_name] = 0
 
-    total_valence_score[president_name] += valence_score
-    total_valence_words[president_name] += 1
+    valence_aggregate[president_name] += valence_score
 
-for name, v_score in total_valence_score.items():
-    print(f"{name}\t{v_score}")
+    
+    if president_name not in valence_words_ct:
+        valence_words_ct[president_name] = 0
 
-for name, total_valence_words in total_valence_score.items():
+    valence_words_ct[president_name] += 1
+
+for name, total_valence_words in valence_words_ct.items():
     print(f"{name}\t{total_valence_words}")
+
+for name, net_v_score in valence_aggregate.items():
+    print(f"{name}\t{net_v_score}")
