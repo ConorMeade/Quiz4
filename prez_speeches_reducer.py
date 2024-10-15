9d00a5a94cb49c5620a5cc58182297c0b923b621
Conor <cmeade6479@gmail.com>
2024-10-15 16:26:59 -0400

    update reducer

--- prez_speeches_reducer.py
+++ prez_speeches_reducer.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
 
-'''prez_speeches_mapper.py'''
+'''prez_speeches_reducer.py'''
 import sys
 
 valence_aggregate = {}
 valence_words_ct = {}
 
-# for line in sys.stdin:
-with open('adams.txt', 'r') as c:
-    for line in c:
-        president_name, valence_score = line.strip().split('\t', 1)
-        try:
-            valence_score  = int(valence_score)
-        except ValueError:
-            # count was not a number, so silently
-            # ignore/discard this line
-            continue
-        if president_name not in valence_aggregate:
-            valence_aggregate[president_name] = 0
+for line in sys.stdin:
+# with open('adams.txt', 'r') as c:
+    president_name, valence_score = line.strip().split('\t', 1)
+    try:
+        valence_score  = int(valence_score)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+    if president_name not in valence_aggregate:
+        valence_aggregate[president_name] = 0
 
-        valence_aggregate[president_name] += valence_score
-        
-        if president_name not in valence_words_ct:
-            valence_words_ct[president_name] = 0
+    valence_aggregate[president_name] += valence_score
+    
+    if president_name not in valence_words_ct:
+        valence_words_ct[president_name] = 0
 
-        valence_words_ct[president_name] += 1
+    valence_words_ct[president_name] += 1
 
-    for name, total_valence_words in valence_words_ct.items():
-        print(f"{name}\t{total_valence_words}")
+for name, total_valence_words in valence_words_ct.items():
+    print(f"{name}\t{total_valence_words}")
 
-    for name, net_v_score in valence_aggregate.items():
-        print(f"{name}\ttotal\t{net_v_score}")
+for name, net_v_score in valence_aggregate.items():
+    print(f"{name}\ttotal\t{net_v_score}")
